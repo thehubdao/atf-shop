f9615eda3c6228680078eb49c4bcfe98f5955521
GabCh15 <gabch15@hotmail.com>
2022-06-24 18:26:32 -0500

    Added notify logic for mobile

--- services/walletService.ts
+++ services/walletService.ts
@@ -1,23 +1,50 @@
 import { SigningType } from '@airgap/beacon-sdk'
 import axios from 'axios'
-const {char2Bytes } = require('@taquito/utils')
+const { char2Bytes } = require('@taquito/utils')
+
+interface loginResult {
+    statusCode: number
+    msg: string
+    token: string
+    refreshToken: string
+}
+
+const notifyMobile = (result: loginResult) => {
+    const aWindow: any = window as any
+    //IOS case
+    if (aWindow.webkit?.messageHandlers?.web3LoginHandler) {
+        aWindow.webkit?.messageHandlers?.web3LoginHandler.postMessage(result)
+    }
+    //Android case
+    if (aWindow.androidWeb3) {
+        aWindow.androidWeb3.onLoginResult(JSON.stringify(result))
+    }
+    console.log(result)
+}
+
 export const getNonce = async (address: any) => {
     console.log(address)
     return (await axios.post('/api/loginGetNonce', { address })).data
 }
 
 export const login = async (address: any, wallet: any) => {
-    let nonce = await getNonce(address)
-   
+    const nonce = await getNonce(address)
     const bytes = char2Bytes(nonce + '')
-    const payloadBytes = '05' + '0100' + char2Bytes(bytes.length+"") + bytes
-    console.log((await axios.post('api/loginWallet', {
-        signature: (
-            await wallet.client.requestSignPayload({
-                signingType: SigningType.MICHELINE,
-                payload: payloadBytes,
-            })
-        ).signature,
-        address: address,
-    })).data)
+    const payloadBytes = '05' + '0100' + char2Bytes(bytes.length + '') + bytes
+    const callData = (
+        await axios.post('api/loginWallet', {
+            signature: (
+                await wallet.client.requestSignPayload({
+                    signingType: SigningType.MICHELINE,
+                    payload: payloadBytes,
+                })
+            ).signature,
+            address: address,
+        })
+    ).data
+    try {
+        notifyMobile(callData)
+    } catch (err) {
+        console.log(err)
+    }
 }
