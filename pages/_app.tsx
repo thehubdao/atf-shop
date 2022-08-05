b614d8d8eee9d51bd26b2fb1a9a1fb66e6b47e12
GabCh15 <gabch15@hotmail.com>
2022-08-05 17:43:59 -0500

    profile build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,8 @@
 
 import type { AppProps } from 'next/app'
-import { Router } from 'next/router';
 import { Provider } from 'react-redux'
 
 import store from '../state/store'
-import Layout from '../components/Layout'
 
 import '../styles/globals.css'
 
@@ -12,9 +10,7 @@ import '../styles/globals.css'
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Layout>
         <Component {...pageProps} />
-      </Layout>
     </Provider>
   )
 }
