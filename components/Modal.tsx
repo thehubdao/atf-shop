03408a2a04eedca9e9e7df3add9026cb932cc44d
GabCh15 <gabch15@hotmail.com>
2022-08-07 20:08:01 -0500

    Buy tokens styling

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -7,7 +7,7 @@ export default function WertModal({walletAddress}:{walletAddress:string}) {
   return (
     <>
       <button
-        className="bg-[#ffe000] text-black active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+        className="bg-[#ffe000] text-black active:bg-yellow font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
         type="button"
         onClick={() => setShowModal(true)}
       >
