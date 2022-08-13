e564feac6398349ca08a7d3315e9bd1c3c9209de
Andres <andgonzalezcas@unal.edu.co>
2022-08-13 16:31:14 -0500

    added control to connect wallet on buy token button

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -10,7 +10,7 @@ interface IModal {
 }
 
 
-export default function WertModal({ title, buttonText, buttonClassName, buttonExtraFunction, body, closeExtraFunction }: IModal) {
+export default function Modal({ title, buttonText, buttonClassName, buttonExtraFunction, body, closeExtraFunction }: IModal) {
   const [showModal, setShowModal] = React.useState(false);
 
   return (
@@ -27,10 +27,10 @@ export default function WertModal({ title, buttonText, buttonClassName, buttonEx
       </button>
       {showModal ? (
         <>
-          <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+          <div className="justify-center items-center flex fixed inset-0 z-50 outline-none focus:outline-none">
             <div className="relative w-auto my-6 mx-auto max-w-3xl h-auto max-h-[700px]">
               {/*content*/}
-              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-[360px] bg-white outline-none focus:outline-none h-full">
+              <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-[360px] sm:w-[500px] bg-white outline-none focus:outline-none h-full">
                 {/*header*/}
                 <div className="flex items-center justify-between p-3 border-b border-solid border-slate-200 rounded-t">
                   <h3 className="text-2xl font-sans">
