6bd0b5336b81298ded70021f98986c8b9c5ccda7
Andres <andgonzalezcas@unal.edu.co>
2022-08-15 08:59:27 -0500

    little visual changes

--- components/modalBodies/Wert.tsx
+++ components/modalBodies/Wert.tsx
@@ -63,7 +63,7 @@ const Wert = ({ isWalletConect }: any) => {
                         <div className='flex flex-col mt-10'>
                             <ConnectWallet
                                 buttonStyle="rounded-full mt-10 bg-[#020202] text-[#FDE100] p-4 cursor-pointer w-44 text-center font-medium self-center"
-                                connectText="Connect Web3 wallet"
+                                containerStyle="flex flex-col pt-0 items-center justify-center space-y-5 font-jost"
                             />
                         </div>
                     </div>
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -14,7 +14,10 @@ const Profile: NextPage = () => {
             <Head>
                 <title>ATF Shop</title>
             </Head>
-            <ConnectWallet buttonStyle="rounded-full mt-10 bg-[#020202] text-[#FDE100] p-4 cursor-pointer w-44 text-center font-medium self-center"/>
+            <ConnectWallet
+                buttonStyle="rounded-full mt-10 bg-[#020202] text-[#FDE100] p-4 cursor-pointer w-44 text-center font-medium self-center"
+                containerStyle="flex flex-col pt-20 items-center justify-center space-y-5 font-jost"
+            />
         </>
     )
 }
