7c0c1c943063cb5ecfade8ddb22c92c50766907e
GabCh15 <gabch15@hotmail.com>
2022-08-23 17:57:45 -0500

    ev

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -12,35 +12,34 @@ import { checkJWT, getUser } from '../services/walletService'
 import Script from 'next/script'
 import { useEffect } from 'react'
 
-
-
 function MyApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
-       (window as any).getAppInfo = async  (options: {
-        os: string
-        token: string
-        refreshToken: string
-    }) => {
-        let { user_id } = (await checkJWT(options.token)).data
-        let { walletAddress } = (await getUser(user_id)).data
-        let walletLogin = {}
-        if (user_id && walletAddress) {
-            //1. Validate if token is valid
-            walletLogin = {
-                // 2. If user does have a wallet, call balances from contracts and show them in the shop.
-                walletAddress,
-                isValidLogin: true,
+        ;(window as any).getAppInfo = async (options: {
+            os: string
+            token: string
+            refreshToken: string
+        }) => {
+            let { user_id } = (await checkJWT(options.token)).data
+            let { walletAddress } = (await getUser(user_id)).data
+            let walletLogin = {}
+            if (user_id && walletAddress) {
+                //1. Validate if token is valid
+                walletLogin = {
+                    // 2. If user does have a wallet, call balances from contracts and show them in the shop.
+                    walletAddress,
+                    isValidLogin: true,
+                }
+            } else {
+                //3. If user doesn't have a wallet -> connect wallet page
+                walletLogin = {
+                    walletAddress,
+                    isValidLogin: false,
+                }
             }
-        } else {
-            //3. If user doesn't have a wallet -> connect wallet page
-            walletLogin = {
-                walletAddress,
-                isValidLogin: false,
-            }
+            ;(window as any).walletLogin = walletLogin
         }
-        ;(window as any).walletLogin = walletLogin
-    } 
-    },[])
+        window.addEventListener('message', (ev) => {console.log(ev,"EVENT")})
+    }, [])
     return (
         <>
             <Head>
