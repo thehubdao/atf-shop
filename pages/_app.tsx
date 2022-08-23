b9472012f21a3d86c36059eee9f1fa651b450d73
GabCh15 <gabch15@hotmail.com>
2022-08-23 17:20:47 -0500

    add getAppInfo function to window

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,37 @@ import '../styles/globals.css'
 import Head from 'next/head'
 import { checkJWT, getUser } from '../services/walletService'
 import Script from 'next/script'
+import { useEffect } from 'react'
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+       (window as any).getAppInfo = async  (options: {
+        os: string
+        token: string
+        refreshToken: string
+    }) => {
+        let { user_id } = (await checkJWT(options.token)).data
+        let { walletAddress } = (await getUser(user_id)).data
+        let walletLogin = {}
+        if (user_id && walletAddress) {
+            //1. Validate if token is valid
+            walletLogin = {
+                // 2. If user does have a wallet, call balances from contracts and show them in the shop.
+                walletAddress,
+                isValidLogin: true,
+            }
+        } else {
+            //3. If user doesn't have a wallet -> connect wallet page
+            walletLogin = {
+                walletAddress,
+                isValidLogin: false,
+            }
+        }
+        ;(window as any).walletLogin = walletLogin
+    } 
+    },[])
     return (
         <>
             <Head>
