b343efbe6b859add1637f3bd1c1b7e536d005a69
GabCh15 <gabch15@hotmail.com>
2022-08-29 13:05:35 -0500

    stringify

--- components/WertWrap.tsx
+++ components/WertWrap.tsx
@@ -4,7 +4,7 @@ import ConnectWallet from './ConnectWallet'
 
 const WertWrap = () => {
     const { user } = useAppSelector((state) => state.account.walletConfig)
-    console.log(user + "USER")
+    console.log(JSON.stringify(user) + "USER")
     console.log("USER")
     return !user.wallet_instance ? (
         <div className="font-jost w-[70%] m-auto text-center my-10">
