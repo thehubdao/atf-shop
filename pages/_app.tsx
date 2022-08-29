b70a07dbf3d4b33a5da58f0ccf7bb81d38ad30f3
GabCh15 <gabch15@hotmail.com>
2022-08-29 12:48:17 -0500

    console.log fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,28 +5,38 @@ import store from '../state/store'
 
 import '../styles/globals.css'
 
-
-
-
 function MyApp({ Component, pageProps }: AppProps) {
     useEffect(() => {
-        (function () {
-            var old = console.log;
-            var logger = document.getElementById('log');
+        ;(function () {
+            var old = console.log
+            var logger = document.getElementById('log')
             console.log = function (message) {
+                let params = arguments
                 if (typeof message == 'object') {
-                    (logger as any).innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+                    ;(logger as any).innerHTML +=
+                        JSON && JSON.stringify
+                            ? JSON.stringify(message)
+                            : message
+                    ;(params as any).forEach((el: any) => {
+                        ;(logger as any).innerHTML +=
+                            ', ' +
+                            (JSON && JSON.stringify ? JSON.stringify(el) : el)
+                    })
+                    ;(logger as any).innerHTML += '<br />'
                 } else {
-                    (logger as any).innerHTML += message + '<br />';
+                    ;(logger as any).innerHTML += message
+                    ;(params as any).forEach((el: any) => {
+                        ;(logger as any).innerHTML += ', ' + el
+                    })
+                    ;(logger as any).innerHTML += '<br />'
                 }
             }
-        })();
+        })()
     })
     return (
         <Provider store={store}>
-
             <Component {...pageProps} />
-            <div id='log' className="overflow-y-auto"></div>
+            <div id="log" className="overflow-y-auto"></div>
         </Provider>
     )
 }
