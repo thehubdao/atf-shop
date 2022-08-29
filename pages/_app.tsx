1c0dcca258963b407c19bb8201184a8872a367a2
GabCh15 <gabch15@hotmail.com>
2022-08-29 12:20:20 -0500

    Console log magicness

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,30 @@
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 import { Provider } from 'react-redux'
 import store from '../state/store'
 
 import '../styles/globals.css'
 
+
+
+
 function MyApp({ Component, pageProps }: AppProps) {
+    useEffect(() => {
+        (function () {
+            var old = console.log;
+            var logger = document.getElementById('log');
+            console.log = function (message) {
+                if (typeof message == 'object') {
+                    (logger as any).innerHTML += (JSON && JSON.stringify ? JSON.stringify(message) : message) + '<br />';
+                } else {
+                    (logger as any).innerHTML += message + '<br />';
+                }
+            }
+        })();
+    })
     return (
         <Provider store={store}>
+            <div id='log'></div>
             <Component {...pageProps} />
         </Provider>
     )
