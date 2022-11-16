3e087ce31071b5e3cd03a1bed7e746a012b7d388
GabCh15 <info@thedac.info>
2022-11-15 22:27:05 -0500

    Add style

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,16 @@ const ConnectWallet = dynamic(() => import('../components/ConnectWallet'), {
     ssr: false,
 }) as any
 const Home: NextPage = () => {
-    
-    return <ConnectWallet/>
+    return (
+        <ConnectWallet
+            containerStyle={
+                'flex flex-col pt-20 items-center justify-center space-y-5 font-jost'
+            }
+            buttonStyle={
+                'rounded-full mt-10 bg-[#020202] text-[#FDE100] p-4 cursor-pointer w-44 text-center font-medium self-center'
+            }
+        />
+    )
 }
 
 export default Home
