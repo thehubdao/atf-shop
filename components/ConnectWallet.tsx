620e8133733a5b240921e8ecb2da515187f3a94d
GabCh15 <info@thedac.info>
2022-11-15 22:17:00 -0500

    Add popup

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from 'react'
 import { useAppDispatch, useAppSelector } from '../state/hooks'
+import { BeaconWallet } from '@taquito/beacon-wallet'
 import {
     connectWallet,
     disconnectWallet,
     _walletConfig,
 } from '../state/walletActions'
-
+import { linkWallet } from '../services/walletService'
+import Popup from './general/Popup'
 
 interface IConnectWallet {
     buttonStyle: string
@@ -19,11 +21,11 @@ const ConnectWallet = ({
     containerStyle,
 }: IConnectWallet) => {
     const dispatch = useAppDispatch()
-    const { user }: any = useAppSelector((state:any) => state.account.walletConfig)
-    const { walletLogin } = useAppSelector(
-        (state:any) => state.walletLogin
-    )
-
+    const { user }: any = useAppSelector((state) => state.account.walletConfig)
+    const [wallet, setWallet] = useState<null | BeaconWallet>(null)
+    const [Tezos, setTezos] = useState(null)
+    const { walletLogin }: any = useAppSelector((state) => state.walletLogin)
+    const [isActive, setIsActive] = useState(false)
     const handleConnectWallet = async () => {
         await dispatch(connectWallet(walletLogin))
     }
@@ -31,17 +33,24 @@ const ConnectWallet = ({
     const handleDisconnectWallet = async () => {
         await dispatch(disconnectWallet())
     }
-    
+
+    useEffect(() => {
+        if (walletLogin?.walletAddress != user?.userAddress) {
+            handleDisconnectWallet()
+        } else if (walletLogin?.walletAddress == user?.userAddress) {
+        }
+    }, [walletLogin])
+
     return (
         <>
-            <div>{user?.walletAddress}</div>
             <div className={containerStyle}>
                 <div
-                    onClick={
+                    onClick={() => {
+                        setIsActive(true)
                         user.wallet_instance
-                            ? handleDisconnectWallet
-                            : handleConnectWallet
-                    }
+                            ? handleDisconnectWallet()
+                            : handleConnectWallet()
+                    }}
                     className={buttonStyle}
                 >
                     {user.wallet_instance
@@ -49,6 +58,21 @@ const ConnectWallet = ({
                         : connectText || 'Connect Wallet'}
                 </div>
             </div>
+            {isActive && (
+                <div className="rounded-full m-auto mt-10  p-4 w-44 cursor-pointer text-center font-medium self-center ">
+                    <Popup
+                        title={'Warning Note'}
+                        message="Please open your Wallet, press connect button and sign the required message. After signing come back to the shop, this may take a moment."
+                        buttonText="OK"
+                        onButtonClick={() => {
+                            setIsActive(false)
+                        }}
+                        onExit={() => {
+                            setIsActive(false)
+                        }}
+                    />
+                </div>
+            )}
         </>
     )
 }
